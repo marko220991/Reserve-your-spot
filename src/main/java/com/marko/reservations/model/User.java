7f5dc978c6f0f6d1205bc054019db7a2606cc26f
Marko Ilic <19marko91.ilic@gmail.com>
2024-01-30 23:15:18 +0100

    Initial commit

---
+++ src/main/java/com/marko/reservations/model/User.java
@@ -0,0 +1,30 @@
+package com.marko.reservations.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "second_name")
+    private String secondName;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+}
