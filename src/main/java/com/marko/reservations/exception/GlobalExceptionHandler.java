0b72d27aafc175df7ad775aec221f59f75dd3639
Marko Ilic <19marko91.ilic@gmail.com>
2024-01-31 11:31:35 +0100

    Refactor GlobalExceptionHandler class

--- src/main/java/com/marko/reservations/exception/GlobalExceptionHandler.java
+++ src/main/java/com/marko/reservations/exception/GlobalExceptionHandler.java
@@ -1,28 +1,24 @@
 package com.marko.reservations.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private Map<String, List<String>> getErrorsMap(List<String> errors) {
-        Map<String, List<String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
-    }
-
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, List<String>>> handleNotFoundException(UserNotFoundException ex) {
-        List<String> errors = Collections.singletonList(ex.getMessage());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> UserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        ErrorResponse message = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<ErrorResponse>(message, HttpStatus.NOT_FOUND);
     }
 }
